92db3448903ee669185d05d0f0715063e6aaf61f
Daryl Rogony <darylrogony@gmail.com>
2022-06-16 10:34:13 +0300

    commit tree ef850da982b97dbf3c5f44401b765228180a48e5

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,31 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT * 
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+  
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
