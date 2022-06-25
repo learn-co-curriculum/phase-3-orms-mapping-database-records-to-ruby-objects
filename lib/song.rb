1b8ed315c8cad7754b9435cfc3202387048d8bec
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-06-25 15:12:08 +0300

    commit tree 9ff62f72f5e41ca9d241656dd7abd7ee15148d37

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,33 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    # self.new is equivalent to Song.new
+    self.new(id: row[0], name: row[1], album: row[2])
+  end
+
+  def self.all
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM songs
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
 end
